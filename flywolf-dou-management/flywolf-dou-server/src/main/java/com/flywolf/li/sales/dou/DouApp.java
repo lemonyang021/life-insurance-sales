9a350380035328f3aac57135b2aebb80f7d6ddd2
flywolf <yangxu021@hotmail.com>
2022-03-25 18:27:58 +0800

    重构模块

---
+++ flywolf-dou-management/flywolf-dou-server/src/main/java/com/flywolf/li/sales/dou/DouApp.java
@@ -0,0 +1,13 @@
+package com.flywolf.li.sales.dou;
+
+/**
+ * Hello world!
+ *
+ */
+public class DouApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
