990b64fb7bea19ab65e57c2f9ddccf9a25217b32
flywolf <yangxu021@hotmail.com>
2022-03-25 12:03:19 +0800

    sales-online功能开发

---
+++ flywolf-sales-online/flywolf-sales-online-channel/src/main/java/com/flywolf/li/sales/online/channel/swagger/ChannelOnlineSwaggerConfiguration.java
@@ -0,0 +1,50 @@
+package com.flywolf.li.sales.online.channel.swagger;
+
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+
+@Configuration
+public class ChannelOnlineSwaggerConfiguration {
+
+    @Bean
+    public GroupedOpenApi channelDocket() {
+        return GroupedOpenApi.builder()
+                .group("channel")
+                .pathsToMatch("/channel/**")
+                .addOperationCustomizer(customGlobalHeaders())
+                .build();
+    }
+
+
+    /**
+     * 给所有@Operation注释的接口添加一个tellerno请求头参数
+     * @return
+     */
+    @Bean
+    public OperationCustomizer customGlobalHeaders() {
+
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+
+            Parameter authorization = new Parameter().in(ParameterIn.HEADER.toString()).name("Authorization")
+                    .description("Authorization details JWT token").example("xxx_token")
+                    .schema(new StringSchema()).required(true);
+
+            Parameter applicationId = new Parameter().in(ParameterIn.HEADER.toString()).schema(new StringSchema())
+                    .name("Application-Id").description("Originating application or client using the service").example("P05")
+                    .required(true);
+
+            operation.addParametersItem(authorization);
+            operation.addParametersItem(applicationId);
+
+            return operation;
+        };
+    }
+}
